fd0ba1d8d08d2ebaf7cc02146e0ff9f971ce9d51
Bruno Lira <brunolyr4@gmail.com>
2016-01-30 20:41:29 -0200

    update

---
+++ AsyncTask_Listener/app/src/androidTest/java/ifpb/edu/br/entidades/Pessoa.java
@@ -0,0 +1,54 @@
+package ifpb.edu.br.entidades;
+
+public class Pessoa {
+
+    private String peso;
+    private String altura;
+    private String Entrevista;
+    private int nivelEsporte;
+    private int nasc;
+
+    public Pessoa(){
+
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getEntrevista() {
+        return Entrevista;
+    }
+
+    public void setEntrevista(String entrevista) {
+        this.Entrevista = entrevista;
+    }
+
+    public int getNivelEsporte() {
+        return nivelEsporte;
+    }
+
+    public void setNivelEsporte(int nivelEsporte) {
+        this.nivelEsporte = nivelEsporte;
+    }
+
+    public int getNasc() {
+        return nasc;
+    }
+
+    public void setNasc(int nasc) {
+        this.nasc = nasc;
+    }
+}
